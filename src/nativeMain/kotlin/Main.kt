cadf7250d96384cdc989c5f7293111fb546cf503
kalemat00 <kalemat@hotmail.it>
2023-01-13 13:20:43 +0100

    initial commit

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,9 +1,6 @@
-fun main() {
-    val i:Int = 1
-    for(i in 1..100 step 2){
-        when{
-            i<50 -> println("smal ${i}")
-            i>=100 -> println("BIG  ${i}")
-        }
-    }
+fun main(){
+    var stringa: String = "23"
+    var integer: Int = 23
+    println(stringa.equals(integer))
+    println(integer.equals(stringa))
 }
